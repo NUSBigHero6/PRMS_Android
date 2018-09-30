9769fc4c9524aae1ced170cf82ada7627b23bf2c
Chen Weng Wei <wwchen21227@gmail.com>
2018-09-30 22:16:35 +0800

    Added date format hint for schedule date

--- app/src/main/java/sg/edu/nus/iss/phoenix/core/android/delegate/DelegateHelper.java
+++ app/src/main/java/sg/edu/nus/iss/phoenix/core/android/delegate/DelegateHelper.java
@@ -1,11 +1,13 @@
 package sg.edu.nus.iss.phoenix.core.android.delegate;
 
 public class DelegateHelper {
+    final static String HOST = "192.168.1.22:8080";
+
     // RESTful parameters.
-    public final static String PRMS_BASE_URL_AUTHENTICATE = "http://192.168.72.166:8080/phoenix/rest/Login/doLogin?";
-    public final static String PRMS_BASE_URL_RADIO_PROGRAM = "http://192.168.72.166:8080/phoenix/rest/radioprogram";
-    public final static String PRMS_BASE_URL_USER = "http://192.168.72.166:8080/phoenix/rest/user";
-    public final static String PRMS_BASE_URL_PROGRAM_SLOT = "http://192.168.72.166:8080/phoenix/rest/schedule";
-    public final static String PRMS_BASE_URL_PRODUCER = "http://192.168.72.166:8080/phoenix/rest/producer";
-    public final static String PRMS_BASE_URL_PRESENTER = "http://192.168.72.166:8080/phoenix/rest/presentor";
+    public final static String PRMS_BASE_URL_AUTHENTICATE = String.format("http://%s/phoenix/rest/Login/doLogin?", HOST);
+    public final static String PRMS_BASE_URL_RADIO_PROGRAM = String.format("http://%s/phoenix/rest/radioprogram", HOST);
+    public final static String PRMS_BASE_URL_USER = String.format("http://%s/phoenix/rest/user", HOST);
+    public final static String PRMS_BASE_URL_PROGRAM_SLOT = String.format("http://%s/phoenix/rest/schedule", HOST);
+    public final static String PRMS_BASE_URL_PRODUCER = String.format("http://%s/phoenix/rest/producer", HOST);
+    public final static String PRMS_BASE_URL_PRESENTER = String.format("http://%s/phoenix/rest/presentor", HOST);
 }
